b7efc4591ceb7ad7371c6f49b5e26de059f85331
Valen <mvalentinacalogeropulos@gmail.com>
2025-10-16 23:31:23 -0300

    proyecto y packages base

---
+++ src/main/java/com/tp4/prog/tp4_facturacion/Tp4_facturacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.tp4.prog.tp4_facturacion;
+
+/**
+ *
+ * @author mvale
+ */
+public class Tp4_facturacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
